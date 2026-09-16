02f1c0fa4e00b17ef8ebb22edec6e8b00c0defff
dnare <david.navarro.reyes.al@iespoligonosur.org>
2026-09-16 16:43:44 +0200

    inicial 1,2

---
+++ UD-1/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        /*File f1=new File("C:\\"); 
+        File f2=new File("f.txt");
+
+        File f3=new File("/home/u/Documentos/ADA/datos","numeros.txt");
+        File f4=new File("/home/u/Documentos/ADA/datos/numeros.txt");
+        File f5=new File(".datos/numeros.txt");  ***** 
+
+
+        System.out.println(f1.getAbsolutePath());
+        System.out.println(f3.exists());
+        System.out.println(f4.exists());
+
+        File[] files = d.listFiles();
+
+        System.out.println(File.separator);
+
+        for(:){}
+        */
+
+        // 1.
+
+        File d=new File("./d");
+        File d1=new File("./d/d1");
+        File d2=new File("./d/d2");
+        File d3=new File("./d/d3");
+        File f11=new File("./d/d1/f11.txt");
+        File f12=new File("./d/d1/f12.txt");
+        File d21=new File("./d/d2/d21");
+        File f21=new File("./d/d2/d21/f21.txt");
+        File d22=new File("./d/d2/d22");
+        File f222=new File("./d/d2/d22/f222");
+        File d31=new File("./d/d3/d31");
+
+        // 2.
+
+        String[] cont_d=d.list();
+
+        // 3. 
+
+        
+        
+    }
+}
